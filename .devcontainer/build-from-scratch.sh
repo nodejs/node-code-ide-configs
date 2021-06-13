cf72c9205b684f3d5c90a4f7e49a5afeec797f42
Andrew Bradley <cspotcode@gmail.com>
2021-06-13 19:55:08 -0400

    update

--- .devcontainer/build-from-scratch.sh
+++ .devcontainer/build-from-scratch.sh
@@ -12,5 +12,5 @@ cd "$__dirname"
 cd ..
 
 docker build -t cspotcode/node-devcontainer:ubuntu-base --file ./.devcontainer/Dockerfile.ubuntu-base .
-# TODO must manually re-tag this image as node-devcontainer:prior to use it in the build-from-prior script
+# TODO must manually re-tag this image as cspotcode/node-devcontainer:prior to use it in the build-from-prior script
 docker build -t cspotcode/node-devcontainer:from-scratch --file ./.devcontainer/Dockerfile.from-scratch .
